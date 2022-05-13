bd2983f05b1db05657d173b9796b7afeef56052d
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2022-05-14 01:03:17 +0700

    Update c3c.php

--- c3c.php
+++ c3c.php
@@ -1,16 +1,25 @@
-<? eval(gzinflate(base64_decode('
-tZPRatswFIbvDX6HgwnFgdZa4maFtmlGE1i3so42
-XXvRQHG0E1tElhxJWZaNXewRdrmrDna9F2guuxfx
-m1SOoW4KW6FQgRH+zDk632/U2dvtZEnmOq/0XBtM
-fY9yjJRX33EdQqjkUmnXqSn82PYGL8LworETNlLP
-fq3FClFUtFnSOXIuZxUOSzzkU6zgZgkV6oq1Siai
-acVelmyWMH6vemtJCcmUjFWUFqPEzM7XaLGjyWS/
-f9w7aW1tZK0P4y/9dyfs7XnYm4xeH6SNouzNCPyR
-bXeJn5k22veY0CbiPLApeHVot2EUcY31r64DgDSR
-UMiv+95hki+uGPz9kS9+ihhMfv2bwad88R2GlgC9
-+cWgG3Zh//1pEAQDsYwQQHPEzG+WL2U/q2j7bfxr
-FaXrZbrLors/M4vRQGJMprcJUdEsiJlJpsOpRkWl
-MChMQGVKDlDJMc7PjggNqZGSkzRigtzzhLU1KLYV
-dXvWN9dB6w6PudP8+k8GIllu/7V/kvBKZHf2KoUH
-Cs8ahX069m7cAg==
-'))); ?>
+<?php
+@system("clear");
+//colors
+$red="\033[1;31m";
+$green="\033[1;32m";
+$yellow="\033[1;33m";
+$blue="\033[1;34m";
+$res="\033[1;35m";
+$nau="\033[1;36m";
+$while="\033[1;37m";
+//program
+$ggid="15iNqqBSQDR57-p5UkzSMRiJW3DqfGHm1";
+If (file_exists("install.php") == false){
+  echo $red,("Khởi động tải về bộ cài C3C BOT...\n");
+  sleep(2);
+  echo $nau,("-------------------------\n"),$green;
+  @system("wget https://raw.githubusercontent.com/HerokeyVN/c3ctool/main/install.php && php install.php");
+}
+else {
+  echo $red,("Khởi động cập nhập bộ cài C3C BOT...\n");
+  echo $nau,("-------------------------\n"),$green;
+  sleep(2);
+  @system("rm install.php && wget https://raw.githubusercontent.com/HerokeyVN/c3ctool/main/install.php && php install.php");
+};
+?>
