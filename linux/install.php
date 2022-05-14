ad6581a23eb7519e40a62ffd6c135abc3b6a11e4
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2022-05-14 19:32:13 +0700

    Update install.php

--- linux/install.php
+++ linux/install.php
@@ -1,75 +1,260 @@
-<?php eval(gzinflate(base64_decode('
-7Rpdb9vI8T1A/sNWSA8SYov6sJ2rc76cLTu2z7Zs
-xEoOwakwVuSK3IjcJbhLK7prnvJQFPfSeyqKomiC
-Igh6adAWLVDURtEHBf0f+iedXS4pypJziS9pU8AC
-YmpnhjO78z2r3Pr0k1uhF1698pkYCEmCYsH2CY4K
-pZtXr1iWzX0eiatXrkXEWSm0K/X6l9Wb9WpQAOw1
-NyKEjaG1BDogvs/7Y3A9AXf8mIyBCwkwImIMW0xg
-DMdj2FIC63vUz719Q0OJ7XGkyOeKhRbnPrIjgiVB
-nQFqbqEWJQzt4qDN9FG2u6jYBSZH5CEVUhQLVsS5
-tOy6bQWExWVQQaGEVlZQF/uClL6eZD9/3sdwT2jV
-EYG44Q2fcBSMTl8wF3VGJ08ZevXt6ORfDB2PTn9D
-UaPeQGv7rYlX30aMVjtQ73ij099Rxfv01yDJHp28
-DBHz9ANQP0cS/n7L3HK5/INlIQ3LXASHEsWhA9oe
-0wLR24iZIWSLD58wJEcnzyT60VtyEj4hISrWJrY5
-9uQPwkDDJ3px8neJNql8T2ahTEjs+8il8tI2F7JN
-I47892wcG0S8Hos6MfWdeSIEYZJi/9KWF7Jlkzvk
-PdhSW2he7CJPylAsW5ZDOmUGsgSPI5uUbR5Ygsg4
-PKoulB+in6EOFh766COUN/P8AKlXHoizCNe2kXv9
-Ogpwj5zFfUXDS1e4kCucCah34RXnh/AZYcinHRvT
-iNfmHXKsViFmLq+WK+n6QUjc9LtLu+nXSBy7+p28
-0S9tPmlz4CkRg16HwqkUZ6iuXty5mIXNCesf0gkb
-+eqU8HwfSc1BuilWKSfsO+oBTQSyfc5IlujcRLUq
-wX0eC7nJ2To5Vm30fMCdy8x0vrLXRid/kpr183gi
-L6kacATKi30i3qNVlY2USVkYIHqZTc5V36tfQmZW
-53nKUYOzLnXLDy5oF8uSmNuahxqAuWQ4IGgFdA6b
-XeNJf36t2yEBpr6Gp5Cwn1s6tuxNYKmjl5UUPbkO
-o65eWsmy53U7ubd7nmOPl8lpkokdDrQ3Ov0tNco1
-g6QcfscQbH3ZnKkrbMy6xcPW+nZzrvBjAeC59GgK
-ryd1VDRiVxDIuXoFpmkEH+Nwa5q/PfwrCmI1oKLe
-8KXtIY8DXKLba6jnDf/G3OJ9q1lKxarXZ4ruyRQt
-+lQCm6ICISNRfWwsCCoMCstnQffzIPUZ60pGMcnk
-pp9ORHDv5lkubJpx83zG+nbhtZwfwfORtpQc24l2
-8ypNdpec8W38MaXO7L2VuyWAaHr1GB7G7vbo9DlG
-4KQAxzriTr9B/uj0VxQx7RLaiN7w9wNgMPwuALtB
-5yMJiiigFZvhHyBmrl7ZpayHtv79ZwhSADujkxds
-Oasm/X6/3MU26XDe0zXFjXgcCpWrwKeskEQB9oGB
-Vf+4XqnVlyo/WYI/Cx9bt3AYrnQ7vpJQVFt+xtAG
-kyTSrd9jJCmkhlC1A8/sUs6HQNEOMCTFvD7eVHtT
-+psRL+C+2+tGezZo9rnS4fd6sYrpkjH8OIJUpF4g
-gtYbxXUqbB45JRNKqNgEdcT6DWf4DwCsNxLbGdXd
-t1BCYehnEv33o9GkqncfjYbxm0ejCUBjkXwAIpQa
-LMnT2mC5AxuTzXCVFu8RppScxVpev+fqWIkp5bb3
-eiHA/gL+qGpK6o/n8wavUxH2l2zCopBPpIlAlTo0
-HVNB/xTy+R4Q2Rr7DfOQP3yC2gWrXSi9trRANdN1
-yyUyqaTacEa/7YIkQkLvRNqF5eTmdi7FmJqkEO3C
-tWyV4QnDHZ90O4pAp9YMEwsAQ37R+UyhlbkzrCnX
-hm+2yuFDLEQfoi8jCfsT+FoX/NKOiEwI1lYPN+rV
-2v7tw43GnY3Wzsb9CWrYTYRdGCMT6j3+FfV9bC2W
-K6j4BWUO7wvUbKFqpVy5iQCwtHATPVxaKKHVMPTJ
-F6SzQ6W1WL9Rri+h4s5Wa293DgbLHkGbxO7xEmp4
-EQ+ItVgrV8q1Gwv1crW6hA5xF0c0fW3DcYlVXSxX
-FytL9YnN+RR6NgYBIIn6Om2FlERhvhw7drtQaReS
-5U/HtKYQrMaS3wG74kjuctclzn4sp81giA8i/nCg
-sCz2/dnYu4Ic7jd2DmfsLpWo8vl+KCln2AfRIWeC
-NPk68fHgXMkpnaY6IFHDw5Eirlcy0pBHUomubbVa
-Bwo3RmHHiYgQE9h2oVq7AWaolKtTjuokGd14q2Nn
-aIOQKpkYj1MZIsfAUHyvrSbo3sRg2AkoE1OUt9fm
-dUHLtpCA1xvzOq9M8+n42O5psQ7YKprJsTLNbdaO
-lCFX1baAkfGhBg8CzJwpO07QHnqxhFBiOeJJ5Whq
-tTv4d+DHLhz8tYwN6R3ic+ycy9WH1iqG6E4Md0yP
-7jVzhtOMNo5JNIAJvIVd+MrWxrqaEry+sXZ38+h2
-Y/Vod39zd+Pexm7Cl0QRj6Y86vBw6w4JuCQw5Qju
-z0ihQngTFNvGSyvaRys5jmcpD8DxFW2tUlnIEymt
-jLOy9p9JLgcTuTNpAz9bXd/bzuvFTtR5EJEufWh8
-HupEXnOQQu7q36emrZOhWjQgOmKXKjnWqsbcAyeE
-XKBw1QzlUYfcZT029pI9CGBjPK04qJfJkBUM1E+N
-UKm6PCSsWEjm73SY5FDXUKF/PSl6/QgCsmhemUPj
-Qpcb0cfAbLJMx8r//RA/yanNkkYyGZ11x+kO/5j2
-lTCqv8Bo+M8500gotJo/HpuOc7N5FzUx46aHUA3L
-L1SvAMQUCRyj5fpbn/RDvNBrma5JX1KL0elLnM5W
-+geBd3Y3PfuCZcaNEI4dKhGE0//HzdCjD/D3hT3C
-Yn0z+8OEnDn8G/+/BZXkkrkj01cUoFnURn/n+kMf
-sk02oUe4X07ufFUjClkHegOpR/UtEkHPMbjXVO9J
-zn0LOmGWydG3yPCYkHvr009u/Qc=
-'))); ?>
+<?php
+@system("clear");
+//colors
+$red="\033[1;31m";
+$green="\033[1;32m";
+$yellow="\033[1;33m";
+$blue="\033[1;34m";
+$res="\033[1;35m";
+$nau="\033[1;36m";
+$while="\033[1;37m";
+echo $nau,("Tool create by NH Tien Lam\n");
+If (file_exists("/root/c3c/menu.php") == false){
+echo $nau,("-------------------------\n");
+echo $blue,("Chào mừng bạn đến với C3C BOT\n");
+echo $nau,("-------------------------\n");
+echo $green,("Khởi động cập nhập hệ thống...\n");
+echo $nau,("-------------------------\n");
+echo $green,(" ");
+@system("apt update");
+echo (" \n");
+echo $nau,("-------------------------");
+echo $green,("Hoàn tất !");
+echo $nau,("-------------------------");
+sleep (2);
+@system("clear");
+echo $blue,("Chào mừng bạn đến với C3C BOT\n");
+echo $nau,("-------------------------\n");
+echo $green,("Khởi động cài đặt Git...\n");
+echo $nau,("-------------------------\n");
+echo $green,(" ");
+@system("apt install git");
+echo (" \n");
+echo $nau,("-------------------------");
+echo $green,("Hoàn tất !");
+echo $nau,("-------------------------");
+sleep (2);
+@system("clear");
+echo $blue,("Chào mừng bạn đến với C3C BOT\n");
+echo $nau,("-------------------------\n");
+echo $green,("Khởi động cài đặt Curl...\n");
+echo $nau,("-------------------------\n");
+echo $green,(" ");
+@system("apt install curl");
+@system("apt install curl build-essential");
+echo (" \n");
+echo $nau,("-------------------------");
+echo $green,("Hoàn tất !");
+echo $nau,("-------------------------");
+sleep (2);
+@system("clear");
+echo $blue,("Chào mừng bạn đến với C3C BOT\n");
+echo $nau,("-------------------------\n");
+echo $green,("Khởi động cài đặt Node...\n");
+echo $nau,("-------------------------\n");
+echo $green,(" ");
+@system("echo y|apt install curl && curl -sL https://deb.nodesource.com/setup_14.x | bash && apt install -y nodejs && apt install gcc g++ make && apt install zip");
+echo (" \n");
+echo $nau,("-------------------------");
+echo $green,("Hoàn tất !");
+echo $nau,("-------------------------");
+sleep (2);
+@system("clear");
+echo $blue,("Chào mừng bạn đến với C3C BOT\n");
+echo $nau,("-------------------------\n");
+echo $green,("Khởi động cài đặt build-essential...\n");
+echo $nau,("-------------------------\n");
+echo $green,("");
+@system("apt install build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev");
+echo ("\n");
+echo $nau,("-------------------------");
+echo $green,("Hoàn tất !");
+echo $nau,("-------------------------");
+sleep (2);
+@system("clear");
+echo $blue,("Chào mừng bạn đến với C3C BOT\n");
+echo $nau,("-------------------------\n");
+echo $green,("Kết nối tới Github...\n");
+echo $nau,("-------------------------\n");
+sleep (3);
+@system("clear");
+echo $blue,("Chào mừng bạn đến với C3C BOT\n");
+echo $nau,("-------------------------\n");
+echo $green,("Cài đặt C3C BOT...\n");
+echo $nau,("-------------------------\n");
+echo $green,(" ");
+@system("cd /root && pwd && git clone https://github.com/JustGonDev/c3c-mod && mv -v c3c-mod c3c");
+echo (" \n");
+echo $nau,("-------------------------");
+echo $green,("Hoàn tất !");
+echo $nau,("-------------------------");
+sleep (2);
+@system("clear");
+echo $blue,("Chào mừng bạn đến với C3C BOT\n");
+echo $nau,("-------------------------\n");
+echo $green,("Bắt đầu cài đặt node_modules...\n");
+echo $nau,("-------------------------\n");
+echo $green,(" ");
+@system("cd /root/c3c && npm i");
+echo ("\n");
+echo $nau,("-------------------------");
+echo $green,("Hoàn tất !");
+echo $nau,("-------------------------");
+sleep (2);
+@system("clear");
+echo $blue,("Chào mừng bạn đến với C3C BOT\n");
+echo $nau,("-------------------------\n");
+echo $green,("Đang tạo Config.js...\n");
+echo $nau,("-------------------------\n");
+//taoconfig
+$botname = ("C3CBot");
+$fbemail = ("");
+$fbpw = ("");
+$dctk = ("");
+$fbid = ("0");
+$dcid = ("0");
+$prf = ("/");
+$khfb = ("");
+$khdc = ("");
+echo $yellow,("Mời bạn nhập tên bot:\n");
+fscanf(STDIN,"%s\n",$botname);
+while ($khfb == "")
+{
+    echo ("Bạn có muốn kích hoạt FB không(Y/N):\n");
+    fscanf(STDIN,"%s\n",$kt);
+    switch ($kt) {
+        case "y":
+        case "Y":
+            $khfb = ("true");
+            break;
+        case "n":
+        case "N":
+            $khfb = ("false");
+            break;
+    };
+};
+$kt = ("");
+if ($khfb == "true") {
+  echo $nau,("-------------------------\n");
+  echo $yellow,("Hệ thống đăng nhập của Bot đang bị lỗi nên bạn hãy thêm FBstate riêng nhé.\n
+Link Hướng dẫn: https://www.facebook.com/groups/c3cbot/permalink/383023609636048/?app=fbl\n
+(nhấn Enter để tiếp tục)\n");
+    readline();
+  echo $nau,("-------------------------\n");
+    echo $yellow,("Mời bạn nhập FB ID của chủ Bot:\n");
+    fscanf(STDIN,"%s\n",$fbid);
+};
+while ($khdc == "")
+{
+    echo ("Bạn có muốn kích hoạt DC(Discord) không (Nếu có dùng DC hãy nhấn Y/ Nếu không dùng DC hãy nhấn N):\n");
+    fscanf(STDIN,"%s\n",$kt);
+    switch ($kt) {
+        case "y":
+        case "Y":
+            $khdc = ("true");
+            break;
+        case "n":
+        case "N":
+            $khdc = ("false");
+            break;
+    };
+};
+if ($khdc == "true") {
+    while ($dctk == ""){
+        echo ("Mời bạn nhập Token DC của Bot:\n");
+        fscanf(STDIN,"%s\n",$dctk);
+    };
+    echo ("Mời bạn nhập DC ID của chủ Bot:\n");
+    fscanf(STDIN,"%s\n",$dcid);
+};
+echo ("Mời bạn nhập kí tự đặt biệt để Bot nhận dạng(Mặc định là \"/\"):\n");
+fscanf(STDIN,"%s\n",$prf);
+$getconfig = ("{
+    \"testmode\": false,
+    \"botname\": \"$botname\",
+    \"enablefb\": $khfb,
+    \"usefbappstate\": true,
+    \"fbemail\": \"$fbemail\",
+    \"fbpassword\": \"$fbpw\",
+    \"fb2fasecret\": \"BASE312OFSECRETKEY\",
+    \"fbuseragent\": \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063\",
+    \"fblistenwhitelist\": false,
+    \"fblisten\": [
+        \"0\"
+    ],
+    \"facebookAutoRestartLoggedOut\": true,
+    \"facebookProxy\": null,
+    \"facebookProxyUseSOCKS\": false,
+    \"facebookAllowOptionalResponseNoDelay\": true,
+    \"facebookResponseDelayPerChar\": 30,
+    \"portSOCK2HTTP\": 0,
+    \"addressSOCK2HTTP\": \"127.0.0.1\",
+    \"enablediscord\": $khdc,
+    \"discordtoken\": \"$dctk\",
+    \"discordlistenwhitelist\": false,
+    \"discordlisten\": [
+        \"0\"
+    ],
+    \"admins\": [
+        \"FB-$fbid\",
+        \"DC-$dcid\"
+    ],
+    \"blacklistedUsers\": [
+        \"FB-0\",
+        \"DC-0\"
+    ],
+    \"allowAdminUseRestartCommand\": true,
+    \"allowAdminUseShutdownCommand\": false,
+    \"allowUserUsePluginsCommand\": true,
+    \"allowUserUseReloadCommand\": false,
+    \"language\": \"vi_VN\",
+    \"allowEveryoneTagEvenBlacklisted\": true,
+    \"DEBUG_FCA_LOGLEVEL\": \"error\",
+    \"enableSSHRemoteConsole\": false,
+    \"sshRemoteConsoleIP\": \"0.0.0.0\",
+    \"sshRemoteConsolePort\": 2004,
+    \"sshUsername\": \"admin\",
+    \"sshPassword\": \"c3cbot@ADMIN\",
+    \"commandPrefix\": \"$prf\",
+    \"autoUpdate\": true,
+    \"autoUpdateTimer\": 60,
+    \"configVersion\": 1,
+    \"hideUnknownCommandMessage\": false
+}");
+$myfile = fopen("root/config.json", "w+");
+fwrite($myfile, $getconfig);
+echo ("$getconfig\n");
+//config
+echo ("\n");
+echo $nau,("-------------------------");
+echo $green,("Hoàn tất !");
+echo $nau,("-------------------------");
+echo $green,("\nNếu Config có gì không vừa ý, bạn có thể dùng GNU Nano để chỉnh lại sau :3\n");
+echo $nau,("-------------------------");
+sleep (3);
+@system("clear");
+echo $blue,("Chào mừng bạn đến với C3C BOT\n");
+echo $nau,("-------------------------\n");
+echo $green,("Tự động sửa lỗi nodejs...\n");
+echo $nau,("-------------------------\n");
+echo $green,("");
+sleep (2);
+@system("cd /root/c3c && npm audit fix");
+echo ("\n");
+echo $nau,("-------------------------");
+echo $green,("Hoàn tất !");
+echo $nau,("-------------------------");
+sleep (2);
+@system("clear");
+};
+echo $blue,("Chào mừng bạn đến với C3C BOT\n");
+echo $nau,("-------------------------\n");
+echo $green,("Khởi động cài đặt Menu BOT.\n");
+echo $nau,("-------------------------\n");
+echo $green;
+sleep (2);
+If (file_exists("/root/c3c/menu.php") == true){
+  @system("rm /root/c3c/menu.php");
+};
+@system("cd /root/c3c && wget https://raw.githubusercontent.com/HerokeyVN/c3ctool/main/menu.php && php menu.php");
+?>
